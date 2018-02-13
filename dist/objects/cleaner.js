170e10e9a1012d62cbbb6fb90bd5bb11931ae8bb
vaberay <10362370+vaberay@users.noreply.github.com>
2018-02-13 15:06:18 -0800

    Build distribution code [EP-1902]

--- dist/objects/cleaner.js
+++ dist/objects/cleaner.js
@@ -2,55 +2,110 @@
 
 // Class that clears out a table within your dynamodb instance
 // Accepts a table name and an array of the key's for the table
-// eg. new DatabaseCleaner('tableName',['key1','key2'])
+// eg. new DatabaseCleaner(dynamo).deleteAllItemsForTable('tableName',['key1','key2'])
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
-var AWS = require("aws-sdk");
-var uuid = require('uuid');
-var dynamo = require('serverless-dynamo-client');
-
 var DatabaseCleaner = function () {
-  function DatabaseCleaner() {
+  function DatabaseCleaner(dynamo) {
     _classCallCheck(this, DatabaseCleaner);
 
-    this.database = dynamo.getDocumentClient({ convertEmptyValues: true });
+    this.dynamo = dynamo;
+    this.database = this.dynamo.getDocumentClient({ convertEmptyValues: true });
   }
 
   _createClass(DatabaseCleaner, [{
-    key: 'deleteAllItemsForTable',
-    value: function deleteAllItemsForTable(tableName, keys, cb) {
+    key: '_scan',
+    value: function _scan(tableName) {
+      var _this = this;
+
       var params = { TableName: tableName, Select: 'ALL_ATTRIBUTES' };
 
-      var database = this.database;
-
-      database.scan(params, function (err, data) {
-        if (!data.Items.length) {
-          return cb ? cb() : undefined;
-        }
-
-        var totalCount = data.Items.length - 1; // Taking into account the zero index
-
-        data.Items.forEach(function (item, index) {
-          var params = { TableName: tableName, Key: {} };
-
-          keys.forEach(function (key) {
-            params.Key[key] = item[key];
-          });
-
-          database.delete(params, function (err, data) {
-            if (err) {
-              return err;
-            }
-            if (totalCount === index) {
-              return cb ? cb() : undefined;
-            }
-          });
+      return new Promise(function (resolve, reject) {
+        _this.database.scan(params, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          resolve(data);
         });
       });
     }
+  }, {
+    key: '_delete',
+    value: function _delete(tableName, item, keys) {
+      var _this2 = this;
+
+      var params = { TableName: tableName, Key: {} };
+      keys.forEach(function (key) {
+        params.Key[key] = item[key];
+      });
+
+      return new Promise(function (resolve, reject) {
+        _this2.database.delete(params, function (err, data) {
+          if (err) {
+            reject(err);
+          }
+          resolve(data);
+        });
+      });
+    }
+  }, {
+    key: 'deleteAllItemsForTable',
+    value: function () {
+      var _ref = _asyncToGenerator( /*#__PURE__*/regeneratorRuntime.mark(function _callee(tableName, keys, cb) {
+        var _this3 = this;
+
+        var data, totalCount;
+        return regeneratorRuntime.wrap(function _callee$(_context) {
+          while (1) {
+            switch (_context.prev = _context.next) {
+              case 0:
+                _context.next = 2;
+                return this._scan(tableName).catch(function (err) {
+                  throw err;
+                });
+
+              case 2:
+                data = _context.sent;
+
+                if (data.Items.length) {
+                  _context.next = 5;
+                  break;
+                }
+
+                return _context.abrupt('return', cb ? cb() : undefined);
+
+              case 5:
+                totalCount = data.Items.length - 1; // Taking into account the zero index
+
+                data.Items.forEach(function (item, index) {
+                  _this3._delete(tableName, item, keys).then(function (data) {
+                    if (totalCount === index) {
+                      return cb ? cb() : undefined;
+                    }
+                  }).catch(function (err) {
+                    throw err;
+                  });
+                });
+
+              case 7:
+              case 'end':
+                return _context.stop();
+            }
+          }
+        }, _callee, this);
+      }));
+
+      function deleteAllItemsForTable(_x, _x2, _x3) {
+        return _ref.apply(this, arguments);
+      }
+
+      return deleteAllItemsForTable;
+    }()
   }]);
 
   return DatabaseCleaner;

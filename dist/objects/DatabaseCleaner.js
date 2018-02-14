9ccdee0048772f3d9ee858f5fe3394abaff81b71
vaberay <10362370+vaberay@users.noreply.github.com>
2018-02-14 15:34:29 -0800

    Rebuild dist file [EP-1902]

--- dist/objects/DatabaseCleaner.js
+++ dist/objects/DatabaseCleaner.js
@@ -1,14 +1,16 @@
 'use strict';
 
-// Class that clears out a table within your dynamodb instance
-// Accepts a table name and an array of the key's for the table
-// eg. new DatabaseCleaner(dynamo).deleteAllItemsForTable('tableName',['key1','key2'])
-
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+require('babel-polyfill');
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+// Class that clears out a table within your dynamodb instance
+// Accepts a table name and an array of the key's for the table
+// eg. new DatabaseCleaner(dynamo).deleteAllItemsForTable('tableName',['key1','key2'])
 
 var DatabaseCleaner = function () {
   function DatabaseCleaner(dynamo) {
